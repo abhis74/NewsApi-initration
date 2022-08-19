6d7968610ff80541467ca142d4cf96d469390f15
kartmax-product-team <abhishekpratap.gh@gmail.com>
2022-08-19 17:22:04 +0530

    first commit

--- src/components/News.js
+++ src/components/News.js
@@ -67,7 +67,7 @@ export class News extends Component {
         {this.state.loading && <Spinner />}
 
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
+          {!this.state.loading &&this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
